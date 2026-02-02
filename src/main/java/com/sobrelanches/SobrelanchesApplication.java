b6abe56cb8dd4458c933b82861da690a02986d91
MoreiraGabryel <marcosgabryel25@gmail.com>
2026-02-02 12:04:58 -0300

    Initial commit

---
+++ src/main/java/com/sobrelanches/SobrelanchesApplication.java
@@ -0,0 +1,13 @@
+package com.sobrelanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobrelanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobrelanchesApplication.class, args);
+	}
+
+}
